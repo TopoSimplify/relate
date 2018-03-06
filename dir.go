f4287eb5ce767efae7f0e8345adf6670600cd59d
Titus Tienaah <titus.intdxdt@gmail.com>
2018-03-06 04:01:23 -0400

    local side decompostion

--- dir.go
+++ dir.go
@@ -3,10 +3,9 @@ package relate
 import (
 	"simplex/ctx"
 	"simplex/node"
-	"simplex/lnr"
 )
 
 //direction relate
-func IsDirRelateValid(self lnr.Polygonal, hull *node.Node, ctx *ctx.ContextGeometry) bool {
+func IsDirRelateValid(hull *node.Node, ctx *ctx.ContextGeometry) bool {
 	return Homotopy(hull.Coordinates(), ctx.Geom)
 }
